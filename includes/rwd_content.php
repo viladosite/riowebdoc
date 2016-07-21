9a65439238548a272c3836f1da130f8fcd87f51c
bruno <bruckmrj@htmailcom>
2016-07-21 14:30:27 -0300

    Estrutura
    criação do esboço da página.

---
+++ includes/rwd_content.php
@@ -0,0 +1,45 @@
+<div class="mdl-grid">
+  		<div class="mdl-cell mdl-cell--12-col">
+  			<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--primary">
+    			<i class="material-icons mdl-badge"> room </i>
+    		</button>
+
+    		<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--primary">
+    			<i class="material-icons mdl-badge"> room </i>
+    		</button>
+
+    		<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--primary">
+    			<i class="material-icons mdl-badge"> room </i>
+    		</button>
+
+    		<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--primary">
+    			<i class="material-icons mdl-badge"> room </i>
+    		</button>
+
+    		<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--primary">
+    			<i class="material-icons mdl-badge"> room </i>
+    		</button>
+
+    		<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--primary">
+    			<i class="material-icons mdl-badge"> room </i>
+    		</button>
+
+    		<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--primary">
+    			<i class="material-icons mdl-badge"> room </i>
+    		</button>
+
+    		<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--primary">
+    			<i class="material-icons mdl-badge"> room </i>
+    		</button>
+
+    		<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--primary">
+    			<i class="material-icons mdl-badge"> room </i>
+    		</button>
+
+  		</div>
+
+  		<div class="mdl-cell mdl-cell--12-col">
+  			conteudo aqui!!
+  		</div>	
+
+ </div> 		
---
+++ includes/rwd_drawer.php
@@ -0,0 +1,28 @@
+<!-- criação do drawer padrão -->
+<div class="mdl-layout__drawer">
+   	<span class="mdl-layout-title">Riowebdoc</span>
+
+<!-- Criação do header do menu com dados do usuário -->
+
+		
+
+
+		<div id="menu" name="menu">
+        <nav class="mdl-navigation">
+
+
+	<a class="mdl-navigation__link" href="#">
+            <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i>
+            Início</a>
+
+    <a class="mdl-navigation__link" href="#">
+            <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">ondemand_video</i>
+            Assistidos</a>        
+
+    <a class="mdl-navigation__link" href="#">
+            <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">thumb_up</i>
+            Gostei</a>   
+
+    	</nav>
+        </div>
+  	</div>
