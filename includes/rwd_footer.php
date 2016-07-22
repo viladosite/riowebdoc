a2761d50dda8e1cc4426ee487b72b9c2a45bbce4
Diogo Fagundes [Vila do Site] <admin@viladosite.com.br>
2016-07-22 15:35:09 -0300

    Inclusão da logo e link do github
    O link e a logo do github foram incluídos no rodapé.

--- includes/rwd_footer.php
+++ includes/rwd_footer.php
@@ -1,7 +1,7 @@
 <!-- Inclusão do mini footer -->
 		<footer class="mdl-mini-footer">
   			<div class="mdl-mini-footer--left-section">
-  				<div class="mdl-logo">icone do gitHub</div>
+  				<div class="mdl-logo"> <a href="http://github.com/viladosite/riowebdoc" target="_blank"> Fork us at <img src="images/github512.png" alt="Github Logo" height="24" width="24"/> </a></div>
   				<ul class="mdl-mini-footer--link-list">
 	  			<li><a href="#">patrocinador</a></li>
   				<li><a href="#">Desenvolvido por</a></li>
