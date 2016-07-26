97675a7549b32c4da57c59c00ddf49b4f0e9a013
bruno <bruckmrj@htmailcom>
2016-07-26 10:40:15 -0300

    tooltip dos pins
    criação de tooltips para os pins de localização

--- includes/rwd_content.php
+++ includes/rwd_content.php
@@ -1,16 +1,26 @@
 <div class="mdl-grid">
   		<div class="rwd_local mdl-cell mdl-cell--12-col">
-  			<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--primary">
+  			<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--primary" id="local1">
     			<i class="material-icons mdl-badge"> room </i>
     		</button>
+				<div class="mdl-tooltip" for="local1">
+				cidade
+				</div>
 
-    		<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--primary">
-    			<i class="material-icons mdl-badge"> room </i>
-    		</button>
 
-    		<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--primary">
+    		<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--primary" id="local2">
     			<i class="material-icons mdl-badge"> room </i>
     		</button>
+    		<div class="mdl-tooltip" for="local2">
+				cidade
+				</div>
+
+    		<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--primary"  id="local3">
+    			<i class="material-icons mdl-badge"> room </i>
+    		</button>
+    		<div class="mdl-tooltip" for="local3">
+				cidade
+				</div>
 
     		<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--primary">
     			<i class="material-icons mdl-badge"> room </i>
