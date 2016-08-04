d5df3894326f53f432b6a413d3920bd55f9f65e4
guzz <gsotero@gmail.com>
2016-08-04 10:47:27 -0300

    vue almost there

--- app/vue/components/media-cloud.vue
+++ app/vue/components/media-cloud.vue
@@ -4,7 +4,7 @@
 
 <template>
 
-  <div id="media_cloud" class="mdl-grid" style="padding: 0; overflow: hidden">
+  <div id="media_cloud" class="mdl-grid" style="padding: 0; overflow: hidden" @mouseout="mouseOut">
     <div class="rwd_content mdl-cell mdl-cell--12-col" style="margin: 0; width: 100%; perspective: 800px;">
 
       <in-media v-for="media in medias" transition="fade" :media="media" :offset="offset"></in-media>
@@ -17,6 +17,7 @@
 <script>
   var $$$ = require('jquery')
   var marked = require('marked')
+  var _ = require('underscore')
   module.exports = {
     replace: true,
     props: ['naves', 'user'],
@@ -26,18 +27,11 @@
         medias: [],
         width: 0,
         height: 0,
-        offset: 0
+        offset: 0,
+        interval: 0
       }
     },
     methods: {
-      flip: function(id) {
-        $$$('#'+id+'-front').css('transform', 'rotateY(180deg)')
-        $$$('#'+id+'-back').css('transform', 'rotateY(0deg)')
-      },
-      unFlip: function(id) {
-        $$$('#'+id+'-back').css('transform', 'rotateY(180deg)')
-        $$$('#'+id+'-front').css('transform', 'rotateY(0deg)')
-      },
       shuffle: function (array) {
         var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -58,11 +52,11 @@
       },
       getSize: function (media) {
         var size = {
-          width: 200,
+          width: 100,
           height: 0
         }
-        var votos = size.width * (media.votes.length/100)
-        var labels = size.width * (media.labels/10)
+        var votos = size.width * (media.votes.length/50)
+        var labels = size.width * media.labels
 
         size.width = size.width + votos + labels
         size.height = (size.width * 9)/16
@@ -72,38 +66,148 @@
         var h = $$$('#markers').outerHeight() + $$$('header').outerHeight() + $$$('footer').outerHeight()
         var w = $$$(window).height()
         var width = $$$(window).width()
-        this.width = width*2
+        this.width = width*3
         this.height = h
-        this.offset = width/2
+        this.offset = - width/2
         $$$('#media_cloud').height(w-h)
       },
-      copyArray: function () {
-        this.medias = this.media_cloud
+      copyArray: function (n) {
+        this.medias = this.media_cloud.slice(0, n+1)
       },
-      checkPos: function(a, array, n) {
+      checkPos: function(a, array, n, t) {
         var ar = array.slice(0, n)
+        var cof = true
+        var toggle = t
+        var intercessoes = []
         var self = this
-        console.log(ar)
-        console.log(n)
         d3.map(ar).each(function(k, v, m) {
           var left = Math.max(a.pos.x, k.x)
           var right = Math.min((a.pos.x + a.size.width), (k.x + k.width))
-          var top = Math.min(a.pos.y, k.y)
-          var bottom = Math.max((a.pos.y + a.size.height), (k.y + k.height))
+          var top = Math.max(a.pos.y, k.y)
+          var bottom = Math.min((a.pos.y + a.size.height), (k.y + k.height))
           a.area = a.size.width * a.size.height 
-          k.area = k.width * k.height 
-          if (left < right && bottom < top) {
+          k.area = k.width * k.height
+          if (left < right && bottom > top) {
             console.log('intercendiu')
-            var int_area = (right - left) * (top - bottom)
-            console.log(int_area)
-            if (k.area * 0.3 < int_area) {
-              if (right - left > top - bottom) {
-                a.pos.x = a.pos.x + (Math.ramdom() * (right - left + 20))
-                self.checkPos(a, array, n)
-              } else if (right - left < top - bottom) {
-                a.pos.y = a.pos.y + (Math.ramdom() * (top - bottom + 20))
-                self.checkPos(a, array, n)
+            var int_area = (right - left) * (bottom - top)
+            intercessoes.push(int_area)
+            // console.log(intercessoes)
+            if (k.area * 0.3 < int_area || a.area * 0.3 < int_area || intercessoes.length > 1 ) {
+              if (right - left < bottom - top && right - left > 1 || toggle === 'x') {
+                if (a.pos.x > k.x && cof) {
+                  cof = false
+                  var o = {
+                    pos: {
+                      x: parseFloat(parseFloat(a.pos.x + (right - left)).toFixed(2)),
+                      y: a.pos.y
+                    },
+                    size: {
+                      width: a.size.width,
+                      height: a.size.height
+                    }
+                  }
+                  if (o.pos.x + o.size.width > self.width) {
+                    console.log(o.pos.x)
+                    console.log(k.width)
+                    o.pos.x = o.pos.x - (k.width*2)
+                    console.log('x maior que '+self.width)
+                    console.log(o.pos.x)
+                  } else {
+                    console.log(right - left)
+                    console.log(a.size.width)
+                    console.log(k.width)
+                    console.log(a.pos.x)
+                    console.log(o.pos.x)
+                    console.log('a1')
+                  }
+                  self.checkPos(o, array, n, 'y')
+                } else if (a.pos.x < k.x && cof) {
+                  cof = false
+                  var o = {
+                    pos: {
+                      x: parseFloat(parseFloat(a.pos.x - (right - left)).toFixed(2)),
+                      y: a.pos.y
+                    },
+                    size: {
+                      width: a.size.width,
+                      height: a.size.height
+                    }
+                  }
+                  if (o.pos.x < 0) {
+                    console.log(o.pos.x)
+                    console.log(k.width)
+                    o.pos.x = o.pos.x + (k.width*2)
+                    console.log('x menor que 0')
+                    console.log(o.pos.x)
+                  } else {
+                    console.log(right - left)
+                    console.log(a.size.width)
+                    console.log(k.width)
+                    console.log(a.pos.x)
+                    console.log(o.pos.x)
+                    console.log('a2')
+                  }
+                  self.checkPos(o, array, n, 'y')
+                }
+              } else if (right - left > bottom - top && bottom - top > 1 || toggle === 'y') {
+                if (a.pos.y > k.y && cof) {
+                  cof = false
+                  var o = {
+                    pos: {
+                      x: a.pos.x,
+                      y: parseFloat(parseFloat(a.pos.y + (bottom - top)).toFixed(2))
+                    },
+                    size: {
+                      width: a.size.width,
+                      height: a.size.height
+                    }
+                  } 
+                  if (o.pos.y + o.size.height > self.height) {
+                    console.log(o.pos.y)
+                    console.log(k.height)
+                    o.pos.y = o.pos.y - k.height
+                    console.log('y maior que '+self.height)
+                    console.log(o.pos.y)
+                  } else {
+                    console.log(bottom - top)
+                    console.log(a.size.height)
+                    console.log(k.height)
+                    console.log(a.pos.y)
+                    console.log(o.pos.y)
+                    console.log('a3')
+                  }
+                  self.checkPos(o, array, n, 'x')
+                } else if (a.pos.y < k.y && cof) {
+                  cof = false
+                  var o = {
+                    pos: {
+                      x: a.pos.x,
+                      y: parseFloat(parseFloat(a.pos.y - (bottom - top)).toFixed(2))
+                    },
+                    size: {
+                      width: a.size.width,
+                      height: a.size.height
+                    }
+                  }
+                  if (o.pos.y < 0) {
+                    console.log(o.pos.y)
+                    console.log(k.height)
+                    o.pos.y = o.pos.y + (k.height*2)
+                    console.log('y menor que 0...')
+                    console.log(o.pos.y)
+                  } else {
+                    console.log(bottom - top)
+                    console.log(a.size.height)
+                    console.log(k.height)
+                    console.log(a.pos.y)
+                    console.log(o.pos.y)
+                    console.log('a4')
+                  }
+                  self.checkPos(o, array, n, 'x')
+                }
               } else {
+                a.pos.y = parseFloat(a.pos.y + bottom - top).toFixed(2)
+                a.pos.x = parseFloat(a.pos.x + right - left).toFixed(2)
                 self.media_cloud[n].x = a.pos.x
                 self.media_cloud[n].y = a.pos.y
               }
@@ -112,9 +216,12 @@
               self.media_cloud[n].y = a.pos.y
             }
           } else {
-            console.log('nao intercendiu')
-            self.media_cloud[n].x = a.pos.x
-            self.media_cloud[n].y = a.pos.y
+            if (cof === true) {
+              console.log('nao intercendiu')
+              // console.log(a.pos)
+              self.media_cloud[n].x = a.pos.x
+              self.media_cloud[n].y = a.pos.y
+            }
           }
         })
       },
@@ -124,7 +231,7 @@
           pos: null
         }
         a.size = {
-          widht: this.media_cloud[n].width,
+          width: this.media_cloud[n].width,
           height: this.media_cloud[n].height
         }
         var pos = {
@@ -137,15 +244,33 @@
           console.log(n)
           this.media_cloud[n].x = a.pos.x
           this.media_cloud[n].y = a.pos.y
-          this.arrangeItens(n+1)
+          this.copyArray(n)
+          // this.arrangeItens(n+1)
         } else if (n!==this.media_cloud.length - 1 && n!== 0) {
-          this.checkPos(a, this.media_cloud, n)
           console.log(n)
-          this.arrangeItens(n+1)
+          this.checkPos(a, this.media_cloud, n, null)
+          this.copyArray(n)
+          // this.arrangeItens(n+1)
         } else if (n===this.media_cloud.length - 1) {
-          this.checkPos(a, this.media_cloud, n)
-          this.copyArray()
+          console.log(n)
+          this.checkPos(a, this.media_cloud, n, null)
+          this.copyArray(n)
         }
+      },
+      mouseMove: function (event) {
+        var self = this
+        var range = d3.scaleLinear()
+                      .domain([0, self.width/2])
+                      .range([0, 2])
+        var interval = -(range(event.clientX) - 1)
+        if (interval > -0.5 && interval < 0.5) {
+          self.interval = 0
+        } else {
+          self.interval = -((range(event.clientX) - 1)*3)
+        }
+      },
+      mouseOut: function (event) {
+        this.interval = 0
       }
     },
     computed: {
@@ -164,13 +289,18 @@
           this.media_cloud.push(m)
         }
       }
-      this.media_cloud = this.shuffle(this.media_cloud)
+      this.media_cloud = _.sortBy(this.shuffle(this.media_cloud), 'width')
+      this.media_cloud.reverse()
 
     },
     attached: function () {
+      var self = this
       componentHandler.upgradeDom()
       this.changeCanvasSize()
       this.arrangeItens(0)
+      window.setInterval(function(){
+        self.offset = self.offset + self.interval
+      }, 1);
     },
     components: {
       'in-media': require('./media.vue')
--- app/vue/components/media.vue
+++ app/vue/components/media.vue
@@ -7,7 +7,6 @@
   <div style="position: absolute; transform-style: preserve-3d; transition: transform 1s;" :style="[{left: offset + media.x+'px'},{top: media.y+'px'}]" class="" :id="media.id">
     <div :id="media.id+'-front'" class="demo-card-wide mdl-card mdl-shadow--2dp" style="position: absolute; backface-visibility: hidden; transform-style: preserve-3d; transition: transform 1s;"  :style="[{height: media.height+'px'},{'min-height': media.height+'px'},{width: media.width+'px'}]">
       <div class="mdl-card__title" :style="{background: 'url('+media.imgs[0]+') center / cover'}" style="height: 100%; color: white;">
-        <h2 class="mdl-card__title-text">{{media.nome}}</h2>
       </div>
       <div class="mdl-card__menu">
         <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect" @click="flip(media.id)">
@@ -16,8 +15,7 @@
       </div>
     </div>
     <div :id="media.id+'-back'" class="demo-card-wide mdl-card mdl-shadow--2dp" style="transform: rotateY( 180deg ); position: absolute; backface-visibility: hidden; transform-style: preserve-3d; transition: transform 1s;" :style="[{height: media.height+'px'},{'min-height': media.height+'px'},{width: media.width+'px'}]">
-      <div class="mdl-card__title" :style="{background: 'url('+media.imgs[0]+') center / cover'}" style="height: 100%; color: white;">
-        <h2 class="mdl-card__title-text">Back</h2>
+      <div class="mdl-card__title" :style="{background: 'url('+media.imgs[1]+') center / cover'}" style="height: 100%; color: white;">
       </div>
       <div class="mdl-card__menu">
         <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect" @click="unFlip(media.id)">
@@ -40,16 +38,15 @@
 
       }
     },
-    watch: {
-      'media': {
-        handler: function (val, oldVal) {
-          console.log(val)
-        },
-        deep: true
-      }
-    },
     methods: {
-
+      flip: function(id) {
+        $$$('#'+id+'-front').css('transform', 'rotateY(180deg)')
+        $$$('#'+id+'-back').css('transform', 'rotateY(0deg)')
+      },
+      unFlip: function(id) {
+        $$$('#'+id+'-back').css('transform', 'rotateY(180deg)')
+        $$$('#'+id+'-front').css('transform', 'rotateY(0deg)')
+      }
     },
     computed: {
       
