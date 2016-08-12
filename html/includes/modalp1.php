7b89702e656f8a5615d8619e7d86cabcf984231b
guzz <gsotero@gmail.com>
2016-08-12 11:22:07 -0300

    Modal 0.1

--- html/includes/modalp1.php
+++ html/includes/modalp1.php
@@ -1,19 +1,19 @@
 <div id="janela2" name="janela2">
-        <div id="menumodal">
-            <a href="includes/modalp2.php" class="steps"> <i class="modaliconsr material-icons">arrow_forward</i> </a>
-        </div>
+    <div id="menumodal">
+        <a href="includes/modalp2.php" class="steps"> <i class="modaliconsr material-icons">arrow_forward</i> </a>
+    </div>
      <br>
      <div class="mdl-grid">
-     <div class="mdl-cell mdl-cell--12-col">   
-    <h3>Passo 1</h3>
-    <p>Escolha 3 vídeos que aparecerão em seu Webcard.</p>
+         <div class="mdl-cell mdl-cell--12-col">   
+            <h3>Passo 1</h3>
+            <p>Escolha 3 vídeos que aparecerão em seu Webcard.</p>
 
-    <div class="video-card"> video </div>
-    <div class="video-card"> video </div>
-    <div class="video-card"> video </div>
-    <div class="video-card"> video </div>
+            <div class="video-card"> video </div>
+            <div class="video-card"> video </div>
+            <div class="video-card"> video </div>
+            <div class="video-card"> video </div>
 
-    </div>
+        </div>
     </div>
 
 </div>
