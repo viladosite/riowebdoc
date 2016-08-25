eb90a27b3a1d8f67dc3ed0796096dc87d00ae468
Diogo Fagundes [Vila do Site] <admin@viladosite.com.br>
2016-08-25 14:36:50 -0300

    Alteração das páginas do modal
    Inclusão do plano de fundo personalizado para os modais e ajustes gerais
    da janela modal.

--- app/vue/components/janela-projeto.vue
+++ app/vue/components/janela-projeto.vue
@@ -3,13 +3,13 @@
 </style>
 
 <template>
-  <div class="mdl-grid">
-    <div class="mdl-cell mdl-cell--12-col">   
-      <h3>O Projeto</h3>
+  <div class="rwd_janela_conteudo">
+      <h3>Projeto</h3>
       <p>Cras ac maximus risus. Donec eget congue justo. Nam quis rutrum nunc, quis finibus nunc. Ut tristique ex sed nulla dapibus, id euismod nunc ultrices. Duis vehicula tempor felis at viverra. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla non purus non turpis dictum fringilla in nec nulla. Duis at pharetra quam. Integer quis ante commodo, tristique quam non, dapibus magna. Integer urna ligula, iaculis eget nisi et, rhoncus sodales metus. Nulla rhoncus, purus id maximus convallis, odio massa volutpat nunc, in tincidunt ante turpis ut nibh.</p>
       <p>In ut mauris nisl. Fusce interdum lobortis velit, et feugiat eros cursus ac. Praesent sit amet odio lacinia, dignissim enim eget, porta neque. In iaculis, sapien vitae placerat ultricies, neque odio vestibulum mi, venenatis sagittis sapien felis varius tellus. Fusce massa lectus, mollis ut molestie sed, posuere vitae eros. In pellentesque diam eu metus rhoncus, eu varius turpis dapibus. Donec commodo urna et erat tristique ultricies. Aliquam molestie orci sed eros lacinia, ac sagittis tellus accumsan.</p>
-
-    </div>
+  </div>
+  <div class="rwd_janela_img">
+    <img src="../img/icon_projeto.png" class="rwd_janela_icon">
   </div>
 </template>
 
